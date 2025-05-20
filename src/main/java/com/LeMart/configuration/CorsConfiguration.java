ee30fdcab780afed2884ac7aae1abc5f7b920527
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2025-05-20 02:27:22 -0400

    Changed frontend url again
    Ec2 changes public ipv4 on restart. added the missing 4200 for port.

--- src/main/java/com/LeMart/configuration/CorsConfiguration.java
+++ src/main/java/com/LeMart/configuration/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                	.allowedOrigins("http://localhost:4200", "http://3.137.162.10")
+                	.allowedOrigins("http://localhost:4200", "http://3.137.162.10:4200")
                     .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true)
